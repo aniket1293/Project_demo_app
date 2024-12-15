edd4aaa60dd87dbf4d5c368d3cb12f3537d58950
Aniket Yadav <anikety12@gmail.com>
2024-12-15 11:45:51 +0000

    Initial Commit

---
+++ dist/controller/NotFound.controller.js
@@ -0,0 +1,2 @@
+sap.ui.define(["./BaseController"],function(e){"use strict";return e.extend("com.sg.Project_demo_app.controller.NotFound",{onLinkPressed:function(){this.getRouter().navTo("worklist")}})});
+//# sourceMappingURL=NotFound.controller.js.map
